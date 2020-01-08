403f61e89912f865b4c3e6338c21afa5e0ccd70b
Promethea Raschke <promethea.raschke@keep.network>
2020-01-08 17:40:25 -0500

    Add branch test

---
+++ implementation/test/branchTest.js
@@ -0,0 +1,19 @@
+const Branch = artifacts.require('./contracts/Branch.sol')
+
+const BN = web3.utils.BN
+
+contract('Branch', (accounts) => {
+    let branchInstance
+
+    before(async () => {
+        branchInstance = await Branch.new()
+    })
+
+    describe('getSlot()', async () => {
+        it('Returns the uint16 in the correct position', async () => {
+            node = new BN('0x0000111122223333444455556666777788889999aaaabbbbccccddddeeeeffff')
+            const result = await branchInstance.getSlot.call(node, 3)
+            assert.equal(result, 0x3333)
+        })
+    })
+})
