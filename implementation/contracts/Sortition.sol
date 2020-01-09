53d2a112e1c3b351a3211850906f0533a841c3c1
Promethea Raschke <promethea.raschke@keep.network>
2020-01-09 11:23:38 -0500

    Add initial insertion logic

--- implementation/contracts/Sortition.sol
+++ implementation/contracts/Sortition.sol
@@ -2,9 +2,13 @@ pragma solidity ^0.5.10;
 pragma experimental ABIEncoderV2;
 
 import './StackLib.sol';
+import './Branch.sol';
+import './Position.sol';
 
 contract Sortition {
     using StackLib for uint256[];
+    using Branch for uint;
+    using Position for uint;
 
     address public owner;
 
@@ -14,7 +18,7 @@ contract Sortition {
     }
 
     // implicit tree
-    uint root;
+    uint public root;
     uint[16] level2;
     uint[256] level3;
     uint[4096] level4;
@@ -29,6 +33,61 @@ contract Sortition {
     uint[][16] hasSpace3;
     uint[][16] hasSpace4;
     uint[][16] hasSpace5;
+
+    function setLeaf(uint leafPosition, address op, uint16 leafWeight) public returns (uint){
+      Leaf memory theLeaf = Leaf({operator: op, weight: leafWeight});
+
+      // set leaf
+      leaves[leafPosition] = theLeaf;
+
+      // set level 5
+      uint childSlot = leafPosition.slot();
+      uint treePosition = leafPosition.parent();
+      uint treeNode = level5[treePosition];
+      uint newNode = treeNode.setSlot(childSlot, leafWeight);
+      level5[treePosition] = newNode;
+      uint16 nodeWeight = uint16(newNode.sumWeight());
+
+      uint node5 = newNode;
+
+      // set level 4
+      childSlot = treePosition.slot();
+      treePosition = treePosition.parent();
+      treeNode = level4[treePosition];
+      newNode = treeNode.setSlot(childSlot, nodeWeight);
+      level4[treePosition] = newNode;
+      nodeWeight = uint16(newNode.sumWeight());
+
+      uint node4 = newNode;
+
+      // set level 3
+      childSlot = treePosition.slot();
+      treePosition = treePosition.parent();
+      treeNode = level3[treePosition];
+      newNode = treeNode.setSlot(childSlot, nodeWeight);
+      level3[treePosition] = newNode;
+      nodeWeight = uint16(newNode.sumWeight());
+
+      uint node3 = newNode;
+
+      // set level 2
+      childSlot = treePosition.slot();
+      treePosition = treePosition.parent();
+      treeNode = level2[treePosition];
+      newNode = treeNode.setSlot(childSlot, nodeWeight);
+      level2[treePosition] = newNode;
+      nodeWeight = uint16(newNode.sumWeight());
+
+      uint node2 = newNode;
+
+      // set level Root
+      childSlot = treePosition.slot();
+      /* uint storage oldRoot = root; */
+      root = root.setSlot(childSlot, nodeWeight);
+      /* oldRoot = newRoot; */
+
+      return root;
+    }
 
    constructor() public {
         owner = msg.sender;
--- implementation/test/sortitionTest.js
+++ implementation/test/sortitionTest.js
@@ -1,4 +1,6 @@
 const Sortition = artifacts.require('./contracts/Sortition.sol')
+const BN = web3.utils.BN
+const toHex = web3.utils.numberToHex
 
 contract('Sortition', (accounts) => {
   let sortitionInstance
@@ -13,4 +15,24 @@ contract('Sortition', (accounts) => {
         assert.equal(owner, accounts[0])
     })
   })
+
+    describe('setLeaf()', async () => {
+        it('Sets the leaf correctly', async () => {
+            let operator1 = accounts[0]
+            let operator2 = accounts[1]
+            let operator3 = accounts[2]
+            let weight1 = new BN('1234', 16)
+            let weight2 = new BN('11', 16)
+            let weight3 = new BN('2', 16)
+            // let operator2 = accounts[1]
+            res1 = await sortitionInstance.setLeaf.call(0xbcdef, operator1, weight1)
+            assert.equal(toHex(res1), '0x12340000000000000000')
+
+            // root = await sortitionInstance.root.call()
+            // assert.equal(toHex(root), '0x12340000000000000000')
+
+            res2 = await sortitionInstance.setLeaf.call(0xfad00, operator2, weight2)
+            assert.equal(toHex(res2), '0x12340000000000000011')
+        })
+    })
 })
