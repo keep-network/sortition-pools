bc206a377dfe2d402abab0b08bce7c24d3c37944
Jakub Nowakowski <jakub.nowakowski@akena.co>
2020-01-20 22:44:33 +0100

    Added tests for SortitionPoolFactory

---
+++ test/factoryTest.js
@@ -0,0 +1,44 @@
+const SortitionPoolFactory = artifacts.require('./contracts/SortitionPoolFactory.sol')
+const SortitionPool = artifacts.require('./contracts/SortitionPool.sol')
+
+contract('SortitionPoolFactory', (accounts) => {
+    const seed = "0xff39d6cca87853892d2854566e883008bc"
+    let sortitionPoolFactory
+
+    before(async () => {
+        sortitionPoolFactory = await SortitionPoolFactory.deployed()
+    })
+
+    describe('createSortitionPool()', async () => {
+        it('emits an event', async () => {
+            const blockNumber = await web3.eth.getBlockNumber()
+
+            const sortitionPoolAddress = await sortitionPoolFactory.createSortitionPool.call()
+            await sortitionPoolFactory.createSortitionPool()
+
+            const eventList = await sortitionPoolFactory.getPastEvents('SortitionPoolCloneCreated', { fromBlock: blockNumber, toBlock: 'latest' })
+            const cloneAddress = eventList[0].returnValues.cloneAddress
+
+            assert.equal(cloneAddress, sortitionPoolAddress);
+        })
+
+        it('creates independent clones', async () => {
+            const sortitionPool1Address = await sortitionPoolFactory.createSortitionPool.call()
+            await sortitionPoolFactory.createSortitionPool()
+            const sortitionPool1 = await SortitionPool.at(sortitionPool1Address)
+
+            const sortitionPool2Address = await sortitionPoolFactory.createSortitionPool.call()
+            await sortitionPoolFactory.createSortitionPool()
+            const sortitionPool2 = await SortitionPool.at(sortitionPool2Address)
+
+            await sortitionPool1.insertOperator(accounts[1], 11)
+            await sortitionPool2.insertOperator(accounts[2], 12)
+
+            let group1 = await sortitionPool1.selectGroup(2, seed)
+            assert.deepEqual(group1, [accounts[1], accounts[1]]);
+
+            let group2 = await sortitionPool2.selectGroup(2, seed)
+            assert.deepEqual(group2, [accounts[2], accounts[2]]);
+        })
+    })
+})
