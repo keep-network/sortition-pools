f8e54bc4e4498f2fd3ae3116aa1bf766d2e514dc
Jakub Nowakowski <jakub.nowakowski@akena.co>
2020-01-27 14:04:44 +0100

    Extracted deployment to a seprate function
    The function can be used by other systems to deploy and link contracts required
    for sortiton pools.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,29 +1,5 @@
-const Sortition = artifacts.require("Sortition");
-const Branch = artifacts.require("Branch");
-const Position = artifacts.require("Position");
-const StackLib = artifacts.require("StackLib");
-const Trunk = artifacts.require("Trunk");
-const Leaf = artifacts.require("Leaf");
-const SortitionPoolFactory = artifacts.require("SortitionPoolFactory");
+const deployContracts = require("./scripts/deployContracts")
 
 module.exports = async function (deployer) {
-  await deployer.deploy(Branch);
-  await deployer.deploy(Position);
-  await deployer.deploy(StackLib);
-  await deployer.deploy(Trunk);
-  await deployer.link(Branch, Leaf);
-  await deployer.deploy(Leaf);
-  await deployer.link(Branch, Sortition);
-  await deployer.link(Position, Sortition);
-  await deployer.link(StackLib, Sortition);
-  await deployer.link(Trunk, Sortition);
-  await deployer.link(Leaf, Sortition);
-  await deployer.deploy(Sortition);
-
-  await deployer.link(Branch, SortitionPoolFactory);
-  await deployer.link(Position, SortitionPoolFactory);
-  await deployer.link(StackLib, SortitionPoolFactory);
-  await deployer.link(Trunk, SortitionPoolFactory);
-  await deployer.link(Leaf, SortitionPoolFactory);
-  await deployer.deploy(SortitionPoolFactory);
+  await deployContracts(artifacts, deployer)
 };
