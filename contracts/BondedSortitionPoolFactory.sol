6b9bc34fa12764cf969dc3f385f8249f3ff381dd
Jakub Nowakowski <jakub.nowakowski@akena.co>
2020-01-31 15:30:59 +0100

    Added factory for BondedSortitionPool contract

---
+++ contracts/BondedSortitionPoolFactory.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.10;
+
+import "./BondedSortitionPool.sol";
+
+/// @title Bonded Sortition Pool Factory
+/// @notice Factory for the creation of new bonded sortition pools.
+contract BondedSortitionPoolFactory {
+    /// @notice Creates a new bonded sortition pool instance.
+    /// @return Address of the new bonded sortition pool contract instance.
+    function createSortitionPool() public returns (address) {
+        return address(new BondedSortitionPool());
+    }
+}
