c1176b32f1d37bb2365c516e24a256a56789df4b
Jakub Nowakowski <jakub.nowakowski@akena.co>
2020-01-31 15:35:53 +0100

    Removed deployment of Sortition contract
    We don't need to deploy Sortition contract as it's implemented by
    SortitionPool and BondedSortitionPool and these two are deployed by
    their factories.

--- migrations/scripts/deployContracts.js
+++ migrations/scripts/deployContracts.js
@@ -1,11 +1,11 @@
 // Deploys contracts using truffle artifacts and deployer.
 module.exports = async function(artifacts, deployer) {
-  const Sortition = artifacts.require('Sortition')
   const Branch = artifacts.require('Branch')
   const Position = artifacts.require('Position')
   const StackLib = artifacts.require('StackLib')
   const Trunk = artifacts.require('Trunk')
   const Leaf = artifacts.require('Leaf')
+
   const SortitionPoolFactory = artifacts.require('SortitionPoolFactory')
   const BondedSortitionPoolFactory = artifacts.require('BondedSortitionPoolFactory')
 
@@ -15,14 +15,6 @@ module.exports = async function(artifacts, deployer) {
   await deployer.deploy(Trunk)
   await deployer.link(Branch, Leaf)
   await deployer.deploy(Leaf)
-
-  // Sortition
-  await deployer.link(Branch, Sortition)
-  await deployer.link(Position, Sortition)
-  await deployer.link(StackLib, Sortition)
-  await deployer.link(Trunk, Sortition)
-  await deployer.link(Leaf, Sortition)
-  await deployer.deploy(Sortition)
 
   // Sortition Pool Factory
   await deployer.link(Branch, SortitionPoolFactory)
