149c2c432b5b4b3dba11aeebcdd71ffce1b810ce
Promethea Raschke <promethea.raschke@keep.network>
2020-02-05 17:10:45 +0000

    Update BondingContractStub to match IBonding

--- test/contracts/BondingContractStub.sol
+++ test/contracts/BondingContractStub.sol
@@ -3,26 +3,9 @@ pragma solidity ^0.5.10;
 contract BondingContractStub {
     function availableUnbondedValue(
         address operator,
-        address bondCreator
+        address bondCreator,
+        address additionalAuthorizedContract
     ) external returns (uint256) {
         return 20000;
-    }
-
-    function createBond(
-        address operator,
-        address holder,
-        uint256 referenceID,
-        uint256 amount
-    ) external {
-        assert(true);
-    }
-
-    function reassignBond(
-        address operator,
-        uint256 referenceID,
-        address newHolder,
-        uint256 newReferenceID
-    ) external {
-        assert(true);
     }
 }
