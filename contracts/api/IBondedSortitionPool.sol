fd7095dac3996fae05116ad32e5851c83b306fea
Promethea Raschke <promethea.raschke@keep.network>
2020-02-05 17:39:29 +0000

    Add sortition pool interfaces to API

---
+++ contracts/api/IBondedSortitionPool.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.10;
+
+interface IBondedSortitionPool {
+    function selectSetGroup(uint256 groupSize, bytes32 seed, uint256 bondSize)
+        public returns (address[] memory selected);
+
+    function isOperatorInPool(address operator) public view returns (bool);
+    function isOperatorUpToDate(address operator) public view returns (bool);
+
+    function joinPool(address operator) public;
+    function updateOperatorWeight(address operator) public;
+    function updateOperatorStatus(address operator) public;
+}
