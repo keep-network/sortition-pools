a73534d98d1c378d794fa7bc9222b13ca89fa38b
Promethea Raschke <promethea.raschke@keep.network>
2020-02-05 16:56:17 +0000

    Standardize bonding interface name and simplify the interface

--- contracts/BondingContractInterface.sol
+++ contracts/BondingContractInterface.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.5.10;
 
-interface BondingContract {
+interface IBonding {
     // Gives the amount of ETH
     // the `operator` has made available for bonding by the `bondCreator`.
     // If the operator doesn't exist,
@@ -8,22 +8,7 @@ interface BondingContract {
     // returns 0.
     function availableUnbondedValue(
         address operator,
-        address bondCreator
+        address bondCreator,
+        address additionalAuthorizedContract
     ) external view returns (uint256);
-
-    // Create a bond, throwing an exception on failure
-    function createBond(
-        address operator,
-        address holder,
-        uint256 referenceID,
-        uint256 amount
-    ) external;
-
-    // Reassign a bond, throwing an exception on failure
-    function reassignBond(
-        address operator,
-        uint256 referenceID,
-        address newHolder,
-        uint256 newReferenceID
-    ) external;
 }
