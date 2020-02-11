097f57b5187a369bdf92b5cbeef3923f9e6c11bc
Promethea Raschke <promethea.raschke@keep.network>
2020-02-11 14:40:51 +0000

    Clean up Trunk internals
    Use a calculated constant trunk size instead of a magic number,
    and use standard CAPITAL_CASE for the constant.

--- contracts/Trunk.sol
+++ contracts/Trunk.sol
@@ -1,15 +1,16 @@
 pragma solidity ^0.5.10;
 
 library Trunk {
-    uint256 constant trunkSize = 65536;
+    // The number of operators a single trunk can hold.
+    uint256 constant TRUNK_SIZE = 2**16;
 
     // the first leaf of the trunk
     function firstLeaf(uint256 trunkN) internal pure returns (uint256) {
-        return trunkSize * trunkN;
+        return TRUNK_SIZE * trunkN;
     }
 
     // the last leaf of the trunk
     function lastLeaf(uint256 trunkN) internal pure returns (uint256) {
-        return trunkSize * (trunkN + 1) - 1;
+        return TRUNK_SIZE * (trunkN + 1) - 1;
     }
 }
