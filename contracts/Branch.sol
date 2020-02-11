b375df20170eccd555c4b4c66198b409daf7beef
Promethea Raschke <promethea.raschke@keep.network>
2020-02-11 14:51:06 +0000

    Remove magic numbers and otherwise clean up Branch.sol

--- contracts/Branch.sol
+++ contracts/Branch.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.5.10;
 /// rely on packing 16 "slots" of 16-bit values into each uint256.
 /// The Branch library permits efficient calculations on these slots.
 library Branch {
+    uint256 constant UINT16_MAX = 2**16 - 1;
 
     /// @notice Calculate the right shift required
     /// to make the 16 least significant bits of an uint256
@@ -13,29 +14,31 @@ library Branch {
     /// @dev Not used for efficiency reasons,
     /// but left to illustrate the meaning of a common pattern.
     /// I wish solidity had macros, even C macros.
-    function slotShift(uint position) internal pure returns (uint) {
+    function slotShift(uint256 position) internal pure returns (uint256) {
         return (15 - position) * 16;
     }
 
     /// @notice Return the `position`th slot of the `node`,
     /// treating `node` as a uint16[16].
-    function getSlot(uint node, uint position) internal pure returns (uint) {
-        uint shiftBits = (15 - position) * 16;
-        // Doing a bitwise AND with `0xffff`
+    function getSlot(uint256 node, uint256 position)
+        internal pure returns (uint256)
+    {
+        uint256 shiftBits = (15 - position) * 16;
+        // Doing a bitwise AND with `UINT16_MAX`
         // clears all but the 16 least significant bits.
         // Because of the right shift by `slotShift(position)` bits,
         // those 16 bits contain the 16 bits in the `position`th slot of `node`.
-        return (node >> shiftBits) & 0xffff;
+        return (node >> shiftBits) & UINT16_MAX;
     }
 
     /// @notice Return `node` with the `position`th slot set to zero.
-    function clearSlot(uint node, uint position)
+    function clearSlot(uint256 node, uint256 position)
         internal
         pure
-        returns (uint)
+        returns (uint256)
     {
-        uint shiftBits = (15 - position) * 16;
-        // Shifting `0xffff` left by `slotShift(position)` bits
+        uint256 shiftBits = (15 - position) * 16;
+        // Shifting `UINT16_MAX` left by `slotShift(position)` bits
         // gives us a number where all bits of the `position`th slot are set,
         // and all other bits are unset.
         //
@@ -46,7 +49,7 @@ library Branch {
         // Bitwise ANDing the original `node` with this number
         // sets the bits of `position`th slot to zero,
         // leaving all other bits unchanged.
-        return node & ~(0xffff << shiftBits);
+        return node & ~(UINT16_MAX << shiftBits);
     }
 
     /// @notice Return `node` with the `position`th slot set to `weight`.
@@ -54,37 +57,37 @@ library Branch {
     /// @param weight The weight of of the node.
     /// Safely truncated to a 16-bit number,
     /// but this should never be called with an overflowing weight regardless.
-    function setSlot(uint node, uint position, uint weight)
+    function setSlot(uint256 node, uint256 position, uint256 weight)
         internal
         pure
-        returns (uint)
+        returns (uint256)
     {
-        uint shiftBits = (15 - position) * 16;
+        uint256 shiftBits = (15 - position) * 16;
         // Clear the `position`th slot like in `clearSlot()`.
-        //
-        // Bitwise AND `weight` with `0xffff`
+        uint256 clearedNode = node & ~(UINT16_MAX << shiftBits);
+        // Bitwise AND `weight` with `UINT16_MAX`
         // to clear all but the 16 least significant bits.
         //
         // Shift this left by `slotShift(position)` bits
         // to obtain a uint256 with all bits unset
         // except in the `position`th slot
         // which contains the 16-bit value of `weight`.
-        //
+        uint256 shiftedWeight = (weight & UINT16_MAX) << shiftBits;
         // When we bitwise OR these together,
         // all other slots except the `position`th one come from the left argument,
         // and the `position`th gets filled with `weight` from the right argument.
-        return node & ~(0xffff << shiftBits) | ((weight & 0xffff) << shiftBits);
+        return clearedNode | shiftedWeight;
     }
 
     /// @notice Calculate the summed weight of all slots in the `node`.
-    function sumWeight(uint node) internal pure returns (uint) {
-        uint sum;
+    function sumWeight(uint256 node) internal pure returns (uint256) {
+        uint256 sum;
 
-        for (uint i = 0; i < 16; i++) {
+        for (uint256 i = 0; i < 16; i++) {
             // Iterate through each slot
             // by shifting `node` right in increments of 16 bits,
             // and adding the 16 least significant bits to the `sum`.
-            sum += (node >> (i * 16)) & 0xffff;
+            sum += (node >> (i * 16)) & UINT16_MAX;
         }
         return sum;
     }
@@ -107,13 +110,13 @@ library Branch {
     function pickWeightedSlot(uint256 node, uint256 index)
         internal
         pure
-        returns (uint slot, uint newIndex)
+        returns (uint256 slot, uint256 newIndex)
     {
-        uint currentSlotWeight;
+        uint256 currentSlotWeight;
         newIndex = index;
 
         for (slot = 0; slot < 16; slot++) {
-            currentSlotWeight = (node >> ((15 - slot) * 16)) & 0xffff;
+            currentSlotWeight = (node >> ((15 - slot) * 16)) & UINT16_MAX;
 
             if (newIndex < currentSlotWeight) {
                 break;
