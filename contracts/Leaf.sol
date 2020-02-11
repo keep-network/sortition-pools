e3ca7c882b7e6115b794effbb7e219e8d472e01e
Promethea Raschke <promethea.raschke@keep.network>
2020-02-11 13:42:32 +0000

    Dramatically reduce gas costs by optimizing Leaf.sol
    Abusing type conversions and finishing off with bitshifts
    is dramatically cheaper than the earlier memory allocation nonsense.
    This change alone makes group selection approximately 50% cheaper
    after accounting for the 21000 gas base transaction fee.

--- contracts/Leaf.sol
+++ contracts/Leaf.sol
@@ -3,42 +3,23 @@ pragma solidity ^0.5.10;
 import "solidity-bytes-utils/contracts/BytesLib.sol";
 
 library Leaf {
-    using BytesLib for bytes;
-
-    function toBytes(uint256 x) internal pure returns (bytes memory) {
-        bytes32 b = bytes32(x);
-        bytes memory c = new bytes(32);
-        for (uint i=0; i < 32; i++) {
-            c[i] = b[i];
-        }
-        return c;
-    }
+    uint256 constant UINT16_MAX = 2**16 - 1;
 
     function make(address operator, uint256 weight)
         internal
         pure
         returns (uint256)
     {
-        bytes memory leafBytes = new bytes(32);
         bytes20 op = bytes20(operator);
-        bytes2 wt = bytes2(uint16(weight));
-
-        for (uint256 i = 0; i < 20; i++) {
-            leafBytes[i] = op[i];
-        }
-
-        for (uint256 j = 0; j < 2; j++) {
-            leafBytes[j + 20] = wt[j];
-        }
-
-        return leafBytes.toUint(0);
+        uint256 wt = weight & UINT16_MAX;
+        return (uint256(bytes32(op)) | wt);
     }
 
     function operator(uint256 leaf) internal pure returns (address) {
-        return toBytes(leaf).toAddress(0);
+        return address(bytes20(bytes32(leaf)));
     }
 
     function weight(uint256 leaf) internal pure returns (uint256) {
-        return uint256(toBytes(leaf).toUint16(20));
+        return (leaf & UINT16_MAX);
     }
 }
