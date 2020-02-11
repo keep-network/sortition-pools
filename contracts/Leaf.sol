e3039a9a904d0ef002fff820a7f50e68e848d5c0
Promethea Raschke <promethea.raschke@keep.network>
2020-02-11 13:48:05 +0000

    Document Leaf optimizations

--- contracts/Leaf.sol
+++ contracts/Leaf.sol
@@ -12,14 +12,22 @@ library Leaf {
     {
         bytes20 op = bytes20(operator);
         uint256 wt = weight & UINT16_MAX;
+        // Converting a bytesX type into a larger type
+        // adds zero bytes on the right.
+        // Bitwise OR them with the 16-bit weight (prepared with bitwise AND)
+        // to get [address || 10 empty bytes || weight]
         return (uint256(bytes32(op)) | wt);
     }
 
     function operator(uint256 leaf) internal pure returns (address) {
+        // Converting a bytesX type into a smaller type
+        // truncates it on the right.
         return address(bytes20(bytes32(leaf)));
     }
 
     function weight(uint256 leaf) internal pure returns (uint256) {
+        // Weight is stored in the 16 least significant bits.
+        // Bitwise AND ensures that we only get the contents of those bits.
         return (leaf & UINT16_MAX);
     }
 }
