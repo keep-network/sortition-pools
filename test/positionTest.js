ca47773cd74a294e05f004bb8fec06cd36ea0070
Promethea Raschke <promethea.raschke@keep.network>
2020-02-12 15:55:11 +0000

    Provide parameters for tests and parameterize positionTest.js

--- test/positionTest.js
+++ test/positionTest.js
@@ -1,6 +1,7 @@
 const Position = artifacts.require('./contracts/Position.sol')
 const PositionStub = artifacts.require('PositionStub.sol')
 const utils = require('./utils')
+const params = require('./params')
 
 const DEPLOY = [
   { name: 'Position', contract: Position },
@@ -8,30 +9,40 @@ const DEPLOY = [
 
 contract('Position', (accounts) => {
   let positionInstance
+  let childPosition
+  let parentPosition
+  let fullPosition
 
   before(async () => {
     deployed = await utils.deploySystem(DEPLOY)
     positionInstance = deployed.PositionStub
+    fullPosition = utils.range(params.levels)
+      .map((n) => ((n + 1) % params.slotCount) << (n * params.slotBits))
+      .reduce(utils.sumReducer)
+    childPosition = 1
+    parentPosition = utils.range(params.levels - 1)
+      .map((n) => ((n + 2) % params.slotCount) << (n * params.slotBits))
+      .reduce(utils.sumReducer)
   })
 
   describe('slot()', async () => {
     it('Returns the last bits', async () => {
-      const result = await positionInstance.slot.call(0x12345)
-      assert.equal(result, 0x5)
+      const result = await positionInstance.slot.call(fullPosition)
+      assert.equal(result, childPosition)
     })
   })
 
   describe('parent()', async () => {
     it('Returns the first bits', async () => {
-      const result = await positionInstance.parent.call(0x12345)
-      assert.equal(result, 0x1234)
+      const result = await positionInstance.parent.call(fullPosition)
+      assert.equal(result, parentPosition)
     })
   })
 
   describe('child()', async () => {
     it('Returns the child address', async () => {
-      const result = await positionInstance.child.call(0x1234, 0x5)
-      assert.equal(result, 0x12345)
+      const result = await positionInstance.child.call(parentPosition, childPosition)
+      assert.equal(result, fullPosition)
     })
   })
 })
