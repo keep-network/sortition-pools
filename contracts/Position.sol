3128ebe817d86db2302c3b945b78c38ad140d4bb
Promethea Raschke <promethea.raschke@keep.network>
2020-02-12 17:26:37 +0000

    Change leaf flag to the most significant bit
    The leaf flag doesn't actually need to be immediately adjacent
    to the position-encoding bits.
    Moving the flag to the most significant bit
    ensures that Position doesn't become an obstacle
    to dynamically parameterized sortition pools.

--- contracts/Position.sol
+++ contracts/Position.sol
@@ -14,9 +14,7 @@ library Position {
     ////////////////////////////////////////////////////////////////////////////
     // Derived constants, do not touch
     uint256 constant SLOT_POINTER_MAX = (2 ** SLOT_BITS) - 1;
-    uint256 constant POSITION_BITS = LEVELS * SLOT_BITS;
-    uint256 constant POSITION_MAX = (2 ** POSITION_BITS) - 1;
-    uint256 constant LEAF_FLAG = 1 << POSITION_BITS;
+    uint256 constant LEAF_FLAG = 1 << 255;
     ////////////////////////////////////////////////////////////////////////////
 
     // Return the last 3 bits of a position number,
@@ -40,7 +38,7 @@ library Position {
     // and the 22nd bit is set as a flag
     // to distinguish the position 0x000000 from an empty field.
     function setFlag(uint256 p) internal pure returns (uint256) {
-        return (p & POSITION_MAX) | LEAF_FLAG;
+        return p | LEAF_FLAG;
     }
 
     // Turn a flagged position into an unflagged position
@@ -50,6 +48,6 @@ library Position {
     // as all position-manipulating code should ignore non-position bits anyway
     // but it's cheap to call so might as well do it.
     function unsetFlag(uint256 p) internal pure returns (uint256) {
-        return p & POSITION_MAX;
+        return p & (~LEAF_FLAG);
     }
 }
