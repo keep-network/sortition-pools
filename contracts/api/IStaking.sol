7dce6d419d8d213559a4b5ac317867c87afc6269
Lukasz Zimnoch <lukasz.zimnoch@keep.network>
2020-02-20 16:36:51 +0100

    Add activeStake function to IStaking

--- contracts/api/IStaking.sol
+++ contracts/api/IStaking.sol
@@ -11,4 +11,15 @@ interface IStaking {
         address operator,
         address operatorContract
     ) external view returns (uint256);
+
+    // Gives the amount of KEEP tokens staked by the `operator`
+    // active in the specified `operatorContract`.
+    //
+    // If the operator doesn't exist or hasn't finished initializing,
+    // or the operator contract hasn't been authorized for the operator,
+    // returns 0.
+    function activeStake(
+        address operator,
+        address operatorContract
+    ) external view returns (uint256);
 }
