28ea5d68d6f6fd0f5c2e214225bb50450c488323
Piotr Dyraga <piotr.dyraga@keep.network>
2020-03-18 14:10:14 +0100

    Added factory for FullyBackedSortitionPool
    Although the factory Solidity code does not do anything clever it lets
    us to avoid linkage hell in projects using sortition pools. We can call
    deployFullyBackedSortitionPoolFactory during migration and that's all.

---
+++ contracts/FullyBackedSortitionPoolFactory.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.5.10;
+
+import "./FullyBackedSortitionPool.sol";
+import "./api/IBonding.sol";
+import "./api/IStaking.sol";
+
+/// @title Fully-Backed Sortition Pool Factory
+/// @notice Factory for the creation of fully-backed sortition pools.
+contract FullyBackedSortitionPoolFactory {
+
+    /// @notice Creates a new fully-backed sortition pool instance.
+    /// @return Address of the new fully-backed sortition pool contract instance.
+    function createSortitionPool(
+        IBonding bondingContract,
+        uint256 minimumStake,
+        uint256 bondWeightDivisor
+    ) public returns (address) {
+        return address(
+            new FullyBackedSortitionPool(
+                bondingContract,
+                minimumStake,
+                bondWeightDivisor,
+                msg.sender
+            )
+        );
+    }
+}
