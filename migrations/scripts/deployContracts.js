b92133080577584c827e6ade0dbede6c89ccd598
Jakub Nowakowski <jakub.nowakowski@akena.co>
2021-01-06 13:58:06 +0100

    Contracts deployer holds libs references
    We want to hold references to deployed libraries instead of deploying
    them again for each factory. Deploying them for each factory caused
    problems with overwritten addresses in artifacts and failures of
    Tenderly publication.

--- migrations/scripts/deployContracts.js
+++ migrations/scripts/deployContracts.js
@@ -1,78 +1,82 @@
 // Deploys contracts using truffle artifacts and deployer.
+class SortitionPoolsDeployer {
+  constructor(deployer, artifacts) {
+    this.deployer = deployer
+    this.artifacts = artifacts
+    this.libs = {}
+  }
 
-async function deployLibraries(artifacts, deployer) {
-  const Branch = artifacts.require("Branch")
-  const Position = artifacts.require("Position")
-  const StackLib = artifacts.require("StackLib")
-  const Leaf = artifacts.require("Leaf")
+  async deployLibraries() {
+    if (!this.libs.Branch) {
+      this.libs.Branch = this.artifacts.require("Branch")
+      await this.deployer.deploy(this.libs.Branch)
+    }
 
-  await deployer.deploy(Branch)
-  await deployer.deploy(Position)
-  await deployer.deploy(StackLib)
-  await deployer.link(Branch, Leaf)
-  await deployer.deploy(Leaf)
+    if (!this.libs.Position) {
+      this.libs.Position = this.artifacts.require("Position")
+      await this.deployer.deploy(this.libs.Position)
+    }
+
+    if (!this.libs.StackLib) {
+      this.libs.StackLib = this.artifacts.require("StackLib")
+      await this.deployer.deploy(this.libs.StackLib)
+    }
+
+    if (!this.libs.Leaf) {
+      this.libs.Leaf = this.artifacts.require("Leaf")
+      await this.deployer.link(this.libs.Branch, this.libs.Leaf)
+      await this.deployer.deploy(this.libs.Leaf)
+    }
+  }
+
+  async deploySortitionPoolFactory() {
+    await this.deployLibraries()
+
+    const SortitionPoolFactory = this.artifacts.require("SortitionPoolFactory")
+
+    // Sortition Pool Factory
+    await this.deployer.link(this.libs.Branch, SortitionPoolFactory)
+    await this.deployer.link(this.libs.Position, SortitionPoolFactory)
+    await this.deployer.link(this.libs.StackLib, SortitionPoolFactory)
+    await this.deployer.link(this.libs.Leaf, SortitionPoolFactory)
+    await this.deployer.deploy(SortitionPoolFactory)
+  }
+
+  async deployBondedSortitionPoolFactory() {
+    await this.deployLibraries()
+
+    const BondedSortitionPoolFactory = this.artifacts.require(
+      "BondedSortitionPoolFactory",
+    )
+
+    // Bonded Sortition Pool Factory
+    await this.deployer.link(this.libs.Branch, BondedSortitionPoolFactory)
+    await this.deployer.link(this.libs.Position, BondedSortitionPoolFactory)
+    await this.deployer.link(this.libs.StackLib, BondedSortitionPoolFactory)
+    await this.deployer.link(this.libs.Leaf, BondedSortitionPoolFactory)
+    await this.deployer.deploy(BondedSortitionPoolFactory)
+  }
+
+  async deployFullyBackedSortitionPoolFactory() {
+    await this.deployLibraries()
+
+    const FullyBackedSortitionPoolFactory = this.artifacts.require(
+      "FullyBackedSortitionPoolFactory",
+    )
+
+    // Fully Backed Sortition Pool Factory
+    await this.deployer.link(this.libs.Branch, FullyBackedSortitionPoolFactory)
+    await this.deployer.link(
+      this.libs.Position,
+      FullyBackedSortitionPoolFactory,
+    )
+    await this.deployer.link(
+      this.libs.StackLib,
+      FullyBackedSortitionPoolFactory,
+    )
+    await this.deployer.link(this.libs.Leaf, FullyBackedSortitionPoolFactory)
+    await this.deployer.deploy(FullyBackedSortitionPoolFactory)
+  }
 }
 
-async function deploySortitionPoolFactory(artifacts, deployer) {
-  await deployLibraries(artifacts, deployer)
-
-  const Branch = artifacts.require("Branch")
-  const Position = artifacts.require("Position")
-  const StackLib = artifacts.require("StackLib")
-  const Leaf = artifacts.require("Leaf")
-
-  const SortitionPoolFactory = artifacts.require("SortitionPoolFactory")
-
-  // Sortition Pool Factory
-  await deployer.link(Branch, SortitionPoolFactory)
-  await deployer.link(Position, SortitionPoolFactory)
-  await deployer.link(StackLib, SortitionPoolFactory)
-  await deployer.link(Leaf, SortitionPoolFactory)
-  await deployer.deploy(SortitionPoolFactory)
-}
-
-async function deployBondedSortitionPoolFactory(artifacts, deployer) {
-  await deployLibraries(artifacts, deployer)
-
-  const Branch = artifacts.require("Branch")
-  const Position = artifacts.require("Position")
-  const StackLib = artifacts.require("StackLib")
-  const Leaf = artifacts.require("Leaf")
-
-  const BondedSortitionPoolFactory = artifacts.require(
-    "BondedSortitionPoolFactory",
-  )
-
-  // Bonded Sortition Pool Factory
-  await deployer.link(Branch, BondedSortitionPoolFactory)
-  await deployer.link(Position, BondedSortitionPoolFactory)
-  await deployer.link(StackLib, BondedSortitionPoolFactory)
-  await deployer.link(Leaf, BondedSortitionPoolFactory)
-  await deployer.deploy(BondedSortitionPoolFactory)
-}
-
-async function deployFullyBackedSortitionPoolFactory(artifacts, deployer) {
-  await deployLibraries(artifacts, deployer)
-
-  const Branch = artifacts.require("Branch")
-  const Position = artifacts.require("Position")
-  const StackLib = artifacts.require("StackLib")
-  const Leaf = artifacts.require("Leaf")
-
-  const FullyBackedSortitionPoolFactory = artifacts.require(
-    "FullyBackedSortitionPoolFactory",
-  )
-
-  // Fully Backed Sortition Pool Factory
-  await deployer.link(Branch, FullyBackedSortitionPoolFactory)
-  await deployer.link(Position, FullyBackedSortitionPoolFactory)
-  await deployer.link(StackLib, FullyBackedSortitionPoolFactory)
-  await deployer.link(Leaf, FullyBackedSortitionPoolFactory)
-  await deployer.deploy(FullyBackedSortitionPoolFactory)
-}
-
-module.exports = {
-  deploySortitionPoolFactory,
-  deployBondedSortitionPoolFactory,
-  deployFullyBackedSortitionPoolFactory,
-}
+module.exports = SortitionPoolsDeployer
