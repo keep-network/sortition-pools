2a02220ee1573debb6f39e28d5cdb510d2821544
Promethea Raschke <promethea.raschke@keep.network>
2021-10-13 14:16:20 +0100

    Unify SortitionPool and AbstractSortitionPool
    With the different types of sortition pools removed,
    the distinction between abstract and concrete pool contracts isn't necessary.

--- contracts/SortitionPool.sol
+++ contracts/SortitionPool.sol
@@ -1,7 +1,8 @@
 pragma solidity 0.5.17;
 
-import "./AbstractSortitionPool.sol";
+import "./DynamicArray.sol";
 import "./RNG.sol";
+import "./SortitionTree.sol";
 import "./api/IStaking.sol";
 
 /// @title Sortition Pool
@@ -15,7 +16,12 @@ import "./api/IStaking.sol";
 /// checked and, if necessary, updated in the sortition pool. If the changes
 /// would be detrimental to the operator, the operator selection is performed
 /// again with the updated input to ensure correctness.
-contract SortitionPool is AbstractSortitionPool {
+contract SortitionPool is SortitionTree {
+  using Leaf for uint256;
+  using Position for uint256;
+  using DynamicArray for DynamicArray.UintArray;
+  using DynamicArray for DynamicArray.AddressArray;
+
   constructor(
     IStaking _stakingContract,
     uint256 _minimumStake,
@@ -39,6 +45,59 @@ contract SortitionPool is AbstractSortitionPool {
 
   PoolParams poolParams;
 
+    // Return whether the operator is eligible for the pool.
+  function isOperatorEligible(address operator) public view returns (bool) {
+    return getEligibleWeight(operator) > 0;
+  }
+
+  // Return whether the operator is present in the pool.
+  function isOperatorInPool(address operator) public view returns (bool) {
+    return getFlaggedLeafPosition(operator) != 0;
+  }
+
+  // Return whether the operator's weight in the pool
+  // matches their eligible weight.
+  function isOperatorUpToDate(address operator) public view returns (bool) {
+    return getEligibleWeight(operator) == getPoolWeight(operator);
+  }
+
+  // Return the weight of the operator in the pool,
+  // which may or may not be out of date.
+  function getPoolWeight(address operator) public view returns (uint256) {
+    uint256 flaggedPosition = getFlaggedLeafPosition(operator);
+    if (flaggedPosition == 0) {
+      return 0;
+    } else {
+      uint256 leafPosition = flaggedPosition.unsetFlag();
+      uint256 leafWeight = getLeafWeight(leafPosition);
+      return leafWeight;
+    }
+  }
+
+  // Add an operator to the pool,
+  // reverting if the operator is already present.
+  function joinPool(address operator) public {
+    uint256 eligibleWeight = getEligibleWeight(operator);
+    require(eligibleWeight > 0, "Operator not eligible");
+
+    insertOperator(operator, eligibleWeight);
+  }
+
+  // Update the operator's weight if present and eligible,
+  // or remove from the pool if present and ineligible.
+  function updateOperatorStatus(address operator) public {
+    uint256 eligibleWeight = getEligibleWeight(operator);
+    uint256 inPoolWeight = getPoolWeight(operator);
+
+    require(eligibleWeight != inPoolWeight, "Operator already up to date");
+
+    if (eligibleWeight == 0) {
+      removeOperator(operator);
+    } else {
+      updateOperator(operator, eligibleWeight);
+    }
+  }
+
   /// @notice Selects a new group of operators of the provided size based on
   /// the provided pseudo-random seed. At least one operator has to be
   /// registered in the pool, otherwise the function fails reverting the
@@ -52,12 +111,30 @@ contract SortitionPool is AbstractSortitionPool {
     uint256 minimumStake
   ) public view returns (address[] memory) {
     require(msg.sender == poolParams.owner, "Only owner may select groups");
-    uint256[] memory selected = generalizedSelectGroup(
-        groupSize, seed
-    );
+
+    uint256 _root = root;
+
+    DynamicArray.UintArray memory selected;
+    selected = DynamicArray.uintArray(groupSize);
+
+    bytes32 rngState = seed;
+    uint256 rngRange = _root.sumWeight();
+    require(rngRange > 0, "Not enough operators in pool");
+    uint256 currentIndex;
+
+    while (selected.array.length < groupSize) {
+        (currentIndex, rngState) = RNG.getIndex(rngRange, rngState);
+
+        uint256 leafPosition = pickWeightedLeaf(currentIndex, _root);
+
+        uint256 leaf = leaves[leafPosition];
+        selected.arrayPush(leaf);
+    }
+
     address[] memory selectedAddresses = new address[](groupSize);
-    for (uint256 i = 0; i < selected.length; i++) {
-      selectedAddresses[i] = selected[i].operator();
+
+    for (uint256 i = 0; i < groupSize; i++) {
+        selectedAddresses[i] = selected.array[i].operator();
     }
     return selectedAddresses;
   }
