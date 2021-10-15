edeee2fdcab887e5ed2e5cdb2d80a1c458c8462e
Promethea Raschke <promethea.raschke@keep.network>
2021-10-15 14:06:41 +0100

    Remove outdated documentation
    The new static sortition pool does not perform any updates during sortition.

--- contracts/SortitionPool.sol
+++ contracts/SortitionPool.sol
@@ -9,13 +9,6 @@ import "./api/IStaking.sol";
 /// @notice A logarithmic data structure used to store the pool of eligible
 /// operators weighted by their stakes. It allows to select a group of operators
 /// based on the provided pseudo-random seed.
-/// @dev Keeping pool up to date cannot be done eagerly as proliferation of
-/// privileged customers could be used to perform DOS attacks by increasing the
-/// cost of such updates. When a sortition pool prospectively selects an
-/// operator, the selected operator’s eligibility status and weight needs to be
-/// checked and, if necessary, updated in the sortition pool. If the changes
-/// would be detrimental to the operator, the operator selection is performed
-/// again with the updated input to ensure correctness.
 contract SortitionPool is SortitionTree {
   using Leaf for uint256;
   using Position for uint256;
