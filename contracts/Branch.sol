654aff5db649aab940be73543daceda9ff294ff5
Promethea Raschke <promethea.raschke@keep.network>
2021-10-19 15:44:29 +0100

    Switch Branch to unchecked math
    Reduce the performance regression from 150% to 50%

--- contracts/Branch.sol
+++ contracts/Branch.sol
@@ -30,7 +30,9 @@ library Branch {
   /// but left to illustrate the meaning of a common pattern.
   /// I wish solidity had macros, even C macros.
   function slotShift(uint256 position) internal pure returns (uint256) {
-    return position * SLOT_WIDTH;
+    unchecked {
+      return position * SLOT_WIDTH;
+    }
   }
 
   /// @notice Return the `position`th slot of the `node`,
@@ -40,12 +42,14 @@ library Branch {
     pure
     returns (uint256)
   {
-    uint256 shiftBits = position * SLOT_WIDTH;
-    // Doing a bitwise AND with `SLOT_MAX`
-    // clears all but the 32 least significant bits.
-    // Because of the right shift by `slotShift(position)` bits,
-    // those 32 bits contain the 32 bits in the `position`th slot of `node`.
-    return (node >> shiftBits) & SLOT_MAX;
+    unchecked {
+      uint256 shiftBits = position * SLOT_WIDTH;
+      // Doing a bitwise AND with `SLOT_MAX`
+      // clears all but the 32 least significant bits.
+      // Because of the right shift by `slotShift(position)` bits,
+      // those 32 bits contain the 32 bits in the `position`th slot of `node`.
+      return (node >> shiftBits) & SLOT_MAX;
+    }
   }
 
   /// @notice Return `node` with the `position`th slot set to zero.
@@ -54,19 +58,21 @@ library Branch {
     pure
     returns (uint256)
   {
-    uint256 shiftBits = position * SLOT_WIDTH;
-    // Shifting `SLOT_MAX` left by `slotShift(position)` bits
-    // gives us a number where all bits of the `position`th slot are set,
-    // and all other bits are unset.
-    //
-    // Using a bitwise NOT on this number,
-    // we get a uint256 where all bits are set
-    // except for those of the `position`th slot.
-    //
-    // Bitwise ANDing the original `node` with this number
-    // sets the bits of `position`th slot to zero,
-    // leaving all other bits unchanged.
-    return node & ~(SLOT_MAX << shiftBits);
+    unchecked {
+      uint256 shiftBits = position * SLOT_WIDTH;
+      // Shifting `SLOT_MAX` left by `slotShift(position)` bits
+      // gives us a number where all bits of the `position`th slot are set,
+      // and all other bits are unset.
+      //
+      // Using a bitwise NOT on this number,
+      // we get a uint256 where all bits are set
+      // except for those of the `position`th slot.
+      //
+      // Bitwise ANDing the original `node` with this number
+      // sets the bits of `position`th slot to zero,
+      // leaving all other bits unchanged.
+      return node & ~(SLOT_MAX << shiftBits);
+    }
   }
 
   /// @notice Return `node` with the `position`th slot set to `weight`.
@@ -79,35 +85,39 @@ library Branch {
     uint256 position,
     uint256 weight
   ) internal pure returns (uint256) {
-    uint256 shiftBits = position * SLOT_WIDTH;
-    // Clear the `position`th slot like in `clearSlot()`.
-    uint256 clearedNode = node & ~(SLOT_MAX << shiftBits);
-    // Bitwise AND `weight` with `SLOT_MAX`
-    // to clear all but the 32 least significant bits.
-    //
-    // Shift this left by `slotShift(position)` bits
-    // to obtain a uint256 with all bits unset
-    // except in the `position`th slot
-    // which contains the 32-bit value of `weight`.
-    uint256 shiftedWeight = (weight & SLOT_MAX) << shiftBits;
-    // When we bitwise OR these together,
-    // all other slots except the `position`th one come from the left argument,
-    // and the `position`th gets filled with `weight` from the right argument.
-    return clearedNode | shiftedWeight;
+    unchecked {
+      uint256 shiftBits = position * SLOT_WIDTH;
+      // Clear the `position`th slot like in `clearSlot()`.
+      uint256 clearedNode = node & ~(SLOT_MAX << shiftBits);
+      // Bitwise AND `weight` with `SLOT_MAX`
+      // to clear all but the 32 least significant bits.
+      //
+      // Shift this left by `slotShift(position)` bits
+      // to obtain a uint256 with all bits unset
+      // except in the `position`th slot
+      // which contains the 32-bit value of `weight`.
+      uint256 shiftedWeight = (weight & SLOT_MAX) << shiftBits;
+      // When we bitwise OR these together,
+      // all other slots except the `position`th one come from the left argument,
+      // and the `position`th gets filled with `weight` from the right argument.
+      return clearedNode | shiftedWeight;
+    }
   }
 
   /// @notice Calculate the summed weight of all slots in the `node`.
   function sumWeight(uint256 node) internal pure returns (uint256 sum) {
-    sum = node & SLOT_MAX;
-    // Iterate through each slot
-    // by shifting `node` right in increments of 32 bits,
-    // and adding the 32 least significant bits to the `sum`.
-    uint256 newNode = node >> SLOT_WIDTH;
-    while (newNode > 0) {
-      sum += (newNode & SLOT_MAX);
-      newNode = newNode >> SLOT_WIDTH;
+    unchecked {
+      sum = node & SLOT_MAX;
+      // Iterate through each slot
+      // by shifting `node` right in increments of 32 bits,
+      // and adding the 32 least significant bits to the `sum`.
+      uint256 newNode = node >> SLOT_WIDTH;
+      while (newNode > 0) {
+        sum += (newNode & SLOT_MAX);
+        newNode = newNode >> SLOT_WIDTH;
+      }
+      return sum;
     }
-    return sum;
   }
 
   /// @notice Pick a slot in `node` that corresponds to `index`.
@@ -130,15 +140,17 @@ library Branch {
     pure
     returns (uint256 slot, uint256 newIndex)
   {
-    newIndex = index;
-    uint256 newNode = node;
-    uint256 currentSlotWeight = newNode & SLOT_MAX;
-    while (newIndex >= currentSlotWeight) {
-      newIndex -= currentSlotWeight;
-      slot++;
-      newNode = newNode >> SLOT_WIDTH;
-      currentSlotWeight = newNode & SLOT_MAX;
+    unchecked {
+      newIndex = index;
+      uint256 newNode = node;
+      uint256 currentSlotWeight = newNode & SLOT_MAX;
+      while (newIndex >= currentSlotWeight) {
+        newIndex -= currentSlotWeight;
+        slot++;
+        newNode = newNode >> SLOT_WIDTH;
+        currentSlotWeight = newNode & SLOT_MAX;
+      }
+      return (slot, newIndex);
     }
-    return (slot, newIndex);
   }
 }
