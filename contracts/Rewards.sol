6f2dc304ec7c66aeaab86370b7dcddf3c906b7a7
Promethea Raschke <promethea.raschke@keep.network>
2021-11-03 13:42:25 +0000

    Document how the pool reward system works internally

--- contracts/Rewards.sol
+++ contracts/Rewards.sol
@@ -1,16 +1,55 @@
 pragma solidity 0.8.6;
 
+/// @title Rewards
+/// @notice Rewards are allocated proportionally to operators
+/// present in the pool at payout based on their weight in the pool.
+///
+/// To facilitate this, we use a global accumulator value
+/// to track the total rewards one unit of weight would've earned
+/// since the creation of the pool.
+///
+/// Whenever a reward is paid, the accumulator is increased
+/// by the size of the reward divided by the total weight
+/// of all eligible operators in the pool.
+///
+/// Each operator has an individual accumulator value,
+/// set to equal the global accumulator when the operator joins the pool.
+/// This accumulator reflects the amount of rewards
+/// that have already been accounted for with that operator.
+///
+/// Whenever an operator's weight in the pool changes,
+/// we can update the amount of rewards the operator has earned
+/// by subtracting the operator's accumulator from the global accumulator.
+/// This gives us the amount of rewards one unit of weight has earned
+/// since the last time the operator's rewards have been updated.
+/// Then we multiply that by the operator's previous (pre-change) weight
+/// to determine how much rewards in total the operator has earned,
+/// and add this to the operator's earned rewards.
+/// Finally, we set the operator's accumulator to the global accumulator value.
 contract Rewards {
   struct OperatorRewards {
+    // The state of the global accumulator
+    // when the operator's rewards were last updated
     uint96 accumulated;
+    // The amount of rewards collected by the operator after the latest update.
+    // The amount the operator could withdraw may equal `available`
+    // or it may be greater, if more rewards have been paid in since then.
     uint96 available;
   }
 
+  // The global accumulator of how much rewards
+  // a hypothetical operator of weight 1 would have earned
+  // since the creation of the pool.
   uint96 internal globalRewardAccumulator;
+  // If the amount of reward tokens paid in
+  // does not divide cleanly by pool weight,
+  // the difference is recorded as rounding dust
+  // and added to the next reward.
   uint96 internal rewardRoundingDust;
 
   mapping(address => OperatorRewards) internal operatorRewards;
 
+  /// @notice Internal function for updating the global state of rewards.
   function addRewards(uint96 rewardAmount, uint256 currentPoolWeight) internal {
     require(currentPoolWeight >= 0, "No recipients in pool");
 
@@ -22,6 +61,7 @@ contract Rewards {
     rewardRoundingDust = newRoundingDust;
   }
 
+  /// @notice Internal function for updating the operator's reward state.
   function updateOperatorRewards(address operator, uint256 oldWeight) internal {
     uint96 acc = globalRewardAccumulator;
     OperatorRewards storage o = operatorRewards[operator];
@@ -30,6 +70,10 @@ contract Rewards {
     o.accumulated = acc;
   }
 
+  /// @notice Set the amount of withdrawable tokens to zero
+  /// and return the previous withdrawable amount.
+  /// @dev Does not update the withdrawable amount,
+  /// but should usually be accompanied by an update.
   function withdrawOperatorRewards(address operator)
     internal
     returns (uint96 withdrawable)
@@ -39,6 +83,9 @@ contract Rewards {
     o.available = 0;
   }
 
+  /// @notice Calculate the amount of tokens the operator could withdraw now
+  /// if its current weight is `oldWeight`.
+  /// Does not update state, but returns what the results would be.
   function availableRewards(address operator, uint256 oldWeight)
     internal
     view
