1c7e435396fadb3959664492e30fa9c334a5dd72
Piotr Dyraga <piotr.dyraga@keep.network>
2022-04-07 15:12:19 +0200

    Proxy reward withdrawal through application contract
    Previously, `SortitionPool.withdrawRewards` could be called directly and
    the beneficiary address was fetched from the staking contract based on
    the operator address.
    
    This is no longer going to work - T `TokenStaking contract` does not store
    operators but staking providers - operator is now a per-application role.
    
    We now expect `SortitionPool.withdrawReward` call to be proxied through the
    application (`RandomBeacon` or `WalletRegistry` contract) that will fetch
    the beneficiary address from T `TokenStaking`.

--- contracts/api/IPoolStaking.sol
+++
@@ -1,7 +0,0 @@
-pragma solidity 0.8.9;
-
-interface IPoolStaking {
-    function rolesOf(
-        address operator
-    ) external view returns (address owner, address beneficiary, address authorizer);
-}
