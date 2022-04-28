0fdffe3a711e69e02bd2aff6dc9bd265e19f9895
Beau Shinkle <beaushinkle@gmail.com>
2022-04-28 11:09:41 -0400

    Refactor the `slot` tests in `positionTest.js`

--- test/positionTest.js
+++ test/positionTest.js
@@ -1,33 +1,62 @@
 const { expect } = require("chai")
-const utils = require("./utils")
-const params = require("./params")
 
 describe("Position", async () => {
   let positionInstance
-  let childPosition
-  let parentPosition
-  let fullPosition
+  const maxPosition = Math.pow(8, 7) // max number of operators
 
   beforeEach(async () => {
     const PositionStub = await ethers.getContractFactory("PositionStub")
     positionInstance = await PositionStub.deploy()
     await positionInstance.deployed()
-
-    fullPosition = utils
-      .range(params.levels)
-      .map((n) => (n + 1) % params.slotCount << (n * params.slotBits))
-      .reduce(utils.sumReducer)
-    childPosition = 1
-    parentPosition = utils
-      .range(params.levels - 1)
-      .map((n) => (n + 2) % params.slotCount << (n * params.slotBits))
-      .reduce(utils.sumReducer)
   })
 
   describe("slot", async () => {
-    it("Returns the last bits", async () => {
-      const result = await positionInstance.slot(fullPosition)
-      expect(result).to.be.equal(childPosition)
+    it("Returns the slot of a node in its parent", async () => {
+      // Sample Cases
+      //
+      // The first position gets the first slot in the first parent, the
+      // second gets the second slot in the first parent, and so on, until
+      // the 9th, which gets the first slot in the second parent. Thus, the
+      // slot is just `position % 8`.
+      const testData = [
+        {
+          position: 196831,
+          slot: 7,
+        },
+        {
+          position: 238968,
+          slot: 0,
+        },
+        {
+          position: 31002,
+          slot: 2,
+        },
+        {
+          position: 1617940,
+          slot: 4,
+        },
+      ]
+      for (let i = 0; i < testData.length; i++) {
+        const test = testData[i]
+        const slot = await positionInstance.slot(test.position)
+        expect(slot).to.equal(
+          test.slot,
+          `unexpected result for test index ${i}`,
+        )
+      }
+
+      // Generative Testing
+      const numSamples = 100
+      for (let i = 0; i < numSamples; i++) {
+        // generate a random position in [0, maxPosition)
+        const position = Math.floor(Math.random() * maxPosition)
+        const slot = await positionInstance.slot(position)
+        const expectation = position % 8
+        expect(slot).to.equal(
+          expectation,
+          `unexpected result for position ${position}`,
+        )
+      }
     })
   })
 
